b3a1047ffead9cfc441a281633f30eb5ea8f6182
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-12-15 08:13:07 -0300

    feat: Cria cadastro de exercícios

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,18 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ExerciseController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     // rotas privadas
+    Route::post('/exercises', [ExerciseController::class, 'store']); //Rota S04
 });
 
 // rota pública de registro de usuário
-Route::post('/users', [UserController::class, 'store']);
+Route::post('/users', [UserController::class, 'store']); //Rota S01
 
 // rota pública de login
-Route::post('/login', [AuthController::class, 'store']);
-
-
+Route::post('/login', [AuthController::class, 'store'])->name('login'); // Rota S02
 
