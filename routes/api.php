d3c7312aea28f0e6eeaf1b0890789e43452b97cd
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-12-17 18:59:24 -0300

    feat: Cria Exportacao de PDF

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\WorkoutController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::middleware('auth:sanctum')->group(function () {
     // rotas privadas
 
